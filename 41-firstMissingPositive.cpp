5d873465362aed0d6a3a03f253149755570907cd
rearcher <568813855@qq.com>
2015-06-11 17:06:34 +0800

    another solution

--- 41-firstMissingPositive.cpp
+++ 41-firstMissingPositive.cpp
@@ -13,3 +13,24 @@ public:
         return ans;
     }
 };
+
+//another solution
+
+int firstMissingPositive(vector<int>& nums) {
+	if(nums.empty()) return 1;
+	for(int i=0; i<nums.size(); i++) {
+		//nums[i]<=nums.size() -------to ensure valid index
+		//nums[nums[i]-1]!=nums[i] --------to avoid endless loop
+		//use while,but not if -----------when do a swap, nums[i] may still need swap
+		while(nums[i]>0 && nums[i]!=i+1 && nums[i]<=nums.size() && nums[nums[i]-1]!=nums[i]) {
+			int temp = nums[i];
+			nums[i] = nums[temp-1];
+			nums[temp-1] = temp;
+		}
+	}
+	int i;
+	for(int i=0; i<nums.size(); i++) {
+		if(nums[i]!=i+1) return i+1;
+	}
+	return i+1;
+}
