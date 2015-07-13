45fa25cef94ce4bade3df124d3318c740639951a
rearcher <568813855@qq.com>
2015-07-13 16:21:34 +0800

    #12

--- 12-intToRoman.cpp
+++ 12-intToRoman.cpp
@@ -1,11 +1,11 @@
 class Solution {
-public:
+private:
+	string M[] = {"", "M", "MM", "MMM"};
+	string C[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+	string X[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+	string I[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+public:	
 	string intToRoman(int num) {
-		if(!num) return "";
-
-		string ans="";
-		int i=num/1000;
-		while(i--) ans += "M";
-	
+		return M[num/1000]+C[num%1000/100]+X[num%100/10]+I[num%10];	
 	}
 }
