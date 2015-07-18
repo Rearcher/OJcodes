fa6763a7c5214a403964c725f2b7f21ca5755300
rearcher <568813855@qq.com>
2015-07-18 13:07:08 +0800

    #109

--- 109-ListToBST.cpp
+++ 109-ListToBST.cpp
@@ -20,3 +20,32 @@ public:
 		return build(head);		
 	}
 };
+
+class Solution { //inorder
+//references:https://leetcode.com/discuss/10924/share-my-code-with-o-n-time-and-o-1-space
+public:
+	ListNode* list;
+	int sizeofList(ListNode* head) {
+		int size=0;
+		while(head) {
+			++size;
+			head = head->next;
+		}
+		return size;
+	}
+
+	TreeNode* generate(int n) {
+		if(!n) return nullptr;
+		TreeNode *node = new TreeNode(0);
+		node->left = generate(n/2);
+		node->val = list->val;
+		list = list->next;
+		node->right = generate(n-n/2-1);
+		return node;
+	}
+
+	TreeNode* sortedListToBST(ListNode *head) {
+		this->list = head;
+		return generate(sizeofList(head));
+	}
+};
