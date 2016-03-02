17bb63da12920da891e0f0445d879f1365475670
rearcher <568813855@qq.com>
2016-03-02 20:54:19 +0800

    maximum depth of binary tree

---
+++ session-2/104.cc
@@ -0,0 +1,17 @@
+/* maximum depth of binary tree */
+#include <iostream>
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+	int maxDepth(TreeNode *root) {
+		if (root == nullptr) return 0;
+		return 1 + max(maxDepth(root->left), maxDepth(root->right));
+	}
+};
