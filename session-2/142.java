1965bce752ce1e649f694630bb51dd22a181ce1d
rearcher <568813855@qq.com>
2016-07-13 14:56:52 +0800

    Linked List Cycle

---
+++ session-2/142.java
@@ -0,0 +1,34 @@
+/**
+ * Linked List Cycle II
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) break;
+        }
+        
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+        
+        fast = head;
+        while (fast != slow) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        
+        return fast;
+    }
+}
