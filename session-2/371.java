d3b404a9a5334c9cc71971d2b8f4830fdc644487
rearcher <568813855@qq.com>
2016-07-14 10:56:12 +0800

    Add Two Integer

---
+++ session-2/371.java
@@ -0,0 +1,15 @@
+// Sum of Two Integers
+public class Solution {
+    public int getSum(int a, int b) {
+        if (a == 0) return b;
+        if (b == 0) return a;
+
+        while (b != 0) {
+            int carry = a & b;
+            a ^= b;
+            b = carry << 1;
+        }
+
+        return a;
+    }
+}
