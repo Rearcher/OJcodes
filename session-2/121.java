00a208bbb20ba837dafc497ed9f03d84cc0e4b5c
rearcher <568813855@qq.com>
2016-07-18 10:21:47 +0800

    Best Time to Buy and Sell Stock

---
+++ session-2/121.java
@@ -0,0 +1,12 @@
+// Best Time to Buy and Sell Stock
+public class Solution {
+    public int maxProfit(int[] prices) {
+        int maxPro = 0;
+        int minPrice = Integer.MAX_VALUE;
+        for (int i = 0; i < prices.length; ++i) {
+            minPrice = Math.min(minPrice, prices[i]);
+            maxPro = Math.max(maxPro, prices[i] - minPrice);
+        }
+        return maxPro;
+    }
+}
