b7acd898d92a9d535c212535ed6c9e7d32b19292
rearcher <568813855@qq.com>
2016-07-20 10:20:50 +0800

    add one solution

--- session-2/153.java
+++ session-2/153.java
@@ -1,6 +1,40 @@
 // Find Minumum in Rotated Sorted Array
 public class Solution {
-    public int findMin(int[] nums) {
+	// origin version
+	public int findMin(int[] nums) {
+        if (nums.length == 1) return nums[0];
+        else if (nums.length == 2) return Math.min(nums[0], nums[1]);
+
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[0] < nums[mid] && nums[mid] < nums[nums.length - 1]) {
+                return nums[0];
+            } else if (nums[0] > nums[mid] && nums[mid] > nums[nums.length - 1]) {
+                return nums[nums.length - 1];
+            }
+
+            if (nums[mid] > nums[0]) {
+                left = mid + 1;
+            } else if (nums[mid] == nums[0]) {
+                return nums[right];
+            } else {
+                if (mid - 1 >= 0 && nums[mid - 1] > nums[mid]) {
+                    return nums[mid];
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return nums[left];
+    }
+
+
+
+
+	// better    
+	public int findMin(int[] nums) {
         int left = 0, right = nums.length - 1;
 
         while (left < right) {
@@ -16,4 +50,6 @@ public class Solution {
         }
         return nums[left];
     }
+
+	
 }
