a82902f91adb3f6def9e0b05c4745241a3949d90
rearcher <568813855@qq.com>
2016-07-26 18:06:35 +0800

    CombinationSum4

---
+++ session-2/377.java
@@ -0,0 +1,15 @@
+// combinationSum4
+public class Solution {
+    public int combinationSum4(int[] nums, int target) {
+       Arrays.sort(nums);
+        int[] res = new int[target + 1];
+        for (int i = 1; i < res.length; ++i) {
+            for (int num : nums) {
+                if (num > i) break;
+                else if (num == i) res[i]++;
+                else res[i] += res[i - num];
+            }
+        }
+        return res[target];
+    }
+}
