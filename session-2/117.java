99063af233b2644cb06d1aeb3e4bd4d84fd11d68
rearcher <568813855@qq.com>
2016-07-28 19:38:34 +0800

    Polulating Next Right Pointers in Each Node II

---
+++ session-2/117.java
@@ -0,0 +1,29 @@
+/**
+ * Polulating Next Right Pointers in Each Node II
+ * Definition for binary tree with next pointer.
+ * public class TreeLinkNode {
+ *     int val;
+ *     TreeLinkNode left, right, next;
+ *     TreeLinkNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void connect(TreeLinkNode root) {
+        while (root != null) {
+            TreeLinkNode tempNode = new TreeLinkNode(0);
+            TreeLinkNode cur = tempNode;
+            while (root != null) {
+                if (root.left != null) {
+                    cur.next = root.left;
+                    cur = cur.next;
+                }
+                if (root.right != null) {
+                    cur.next = root.right;
+                    cur = cur.next;
+                }
+                root = root.next;
+            }
+            root = tempNode.next;
+        }
+    }
+}
