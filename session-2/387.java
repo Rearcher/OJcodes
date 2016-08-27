b7639b7ffc94a90ccaebf1458c656dca08afd7cd
rearcher <568813855@qq.com>
2016-08-27 18:00:45 +0800

    #387 first unique character in a string

---
+++ session-2/387.java
@@ -0,0 +1,13 @@
+// first Unique char
+public class Solution {
+    public int firstUniqChar(String s) {
+        int[] cnt = new int[26];
+        for (int i = 0; i < s.length(); ++i)
+            cnt[s.charAt(i) - 'a']++;
+
+        for (int i = 0; i < s.length(); ++i)
+            if (cnt[s.charAt(i) - 'a'] == 1) return i;
+        return -1;
+
+    }
+}
