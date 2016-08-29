c6e0f4b3eb05441c58bd158e0f9fee4c0dbd15dc
rearcher <568813855@qq.com>
2016-08-29 21:03:23 +0800

    #224 Basic Calculator

---
+++ session-2/224.java
@@ -0,0 +1,30 @@
+// Basic Calculator
+public class Solution {
+    public int calculate(String s) {
+        Stack<Integer> stack = new Stack<>();
+        int result = 0, sign = 1;
+
+        for (int i = 0; i < s.length(); ++i) {
+            if (Character.isDigit(s.charAt(i))) {
+                int sum = s.charAt(i) - '0';
+                while (i + 1 < s.length() && Character.isDigit(s.charAt(i + 1))) {
+                    sum = sum * 10 + s.charAt(i + 1) - '0';
+                    i++;
+                }
+                result += sum * sign;
+            } else if (s.charAt(i) == '+') {
+                sign = 1;
+            } else if (s.charAt(i) == '-') {
+                sign = -1;
+            } else if (s.charAt(i) == '(') {
+                stack.push(result);
+                stack.push(sign);
+                result = 0;
+                sign = 1;
+            } else if (s.charAt(i) == ')') {
+                result = result * stack.pop() + stack.pop();
+            }
+        }
+        return result;
+    }
+}
