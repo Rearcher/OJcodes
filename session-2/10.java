cf26a2ec3cbfcb72edf843983b2c3cb2f2f4fb5d
rearcher <568813855@qq.com>
2016-09-09 14:18:24 +0800

    #10* Regular Expression Matching

---
+++ session-2/10.java
@@ -0,0 +1,29 @@
+/* 10. Regular Expression Matching
+ * dp[i][j] = true means s[0...i-1] matches p[0..j-1]
+ * if p[j-1] != '*'
+ *     dp[i][j] = dp[i-1][j-1] && s[i-1] == p[j-1]
+ * else
+ *     dp[i][j] = dp[i][j-2] or     // this means x* match x 0 time
+ *                dp[i-1][j] && s[i-1] = p[j-2]   // this means x* match x at least 1 time
+ */ 
+public class Solution {
+    public boolean isMatch(String s, String p) {
+        int m = s.length(), n = p.length();
+        boolean dp[][] = new boolean[m+1][n+1];
+
+        dp[0][0] = true;
+        for (int j = 1; j <= n; ++j)
+            dp[0][j] = j > 1 && '*' == p.charAt(j-1) && dp[0][j-2];
+
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (p.charAt(j-1) != '*')
+                    dp[i][j] = dp[i-1][j-1] && (s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '.');
+                else
+                    dp[i][j] = dp[i][j-2] || ((s.charAt(i-1) == p.charAt(j-2) || p.charAt(j-2) == '.') && dp[i-1][j]);
+            }
+        }
+        
+        return dp[m][n];
+    }
+}
