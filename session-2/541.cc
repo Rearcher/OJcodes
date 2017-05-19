31f25adad61b2ded918b2e080b86399fcb6feb17
rahul <568813855@qq.com>
2017-05-19 20:21:11 +0800

    541. Reverse String II (fucking c++ substr)

---
+++ session-2/541.cc
@@ -0,0 +1,33 @@
+// 541. Reverse String II
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        string result = "";
+        int i = 0, n = s.size();
+        while (i < n) {
+            string temp = "";
+            temp = i + k <= n ? s.substr(i, k) : s.substr(i);
+            reverse(temp.begin(), temp.end());
+            result.append(string(temp));
+            i = i + k <= n ? i + k : n;
+
+            if (i >= n) break;
+            temp = i + k <= n ? s.substr(i, k) : s.substr(i);
+            cout << i + k << " " << n << endl;
+            result.append(string(temp));
+            i = i + k <= n ? i + k : n;
+        }
+        return result;
+    }
+};
+
+int main() {
+    Solution s;
+    cout << s.reverseStr("abcdefg", 3);
+    return 0;
+}
