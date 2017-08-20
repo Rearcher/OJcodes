b02e592e213a232912a468de31323e96a1d8c0a0
rahul <568813855@qq.com>
2017-08-20 15:39:10 +0800

    239. Sliding Window Maximum

---
+++ session-2/239.cpp
@@ -0,0 +1,28 @@
+// 239. Sliding Window Maximum
+#include "leetcode.hpp"
+using namespace std;
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> dq;
+        vector<int> result;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (!dq.empty() && dq.front() == i - k)
+                dq.pop_front();
+            while (!dq.empty() && nums[dq.back()] < nums[i])
+                dq.pop_back();
+            dq.push_back(i);
+            if (i >= k - 1) result.push_back(nums[dq.front()]);
+        }
+        return result;
+    }
+};
+
+int main() {
+    vector<int> nums{1, 3, -1, -3, 5, 3, 6, 7};
+    Solution s;
+    vector<int> result = s.maxSlidingWindow(nums, 3);
+    for (int n : result) cout << n << " ";
+    cout << "\n";
+}
