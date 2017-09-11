82616f2aee5d65901580779d4c13034a9cf95514
rahul <568813855@qq.com>
2017-09-11 14:06:56 +0800

    335. Self Crossing

---
+++ session-2/335.cpp
@@ -0,0 +1,19 @@
+// 335. Self Crossing
+#include "leetcode.hpp"
+using namespace std;
+
+class Solution {
+public:
+    bool isSelfCrossing(vector<int>& x) {
+        for(int i = 3; i < x.size(); i++) {
+            if(x[i-1] <= x[i-3] && x[i] >= x[i-2]) return true;
+            if(i > 3 && x[i] + x[i-4] >= x[i-2] && x[i-1] == x[i-3]) return true;
+            if(i > 4 && x[i-1] + x[i-5] >= x[i-3] && x[i-1] <= x[i-3] && x[i] + x[i-4] >= x[i-2] && x[i-4] <= x[i-2]) return true;
+        }
+        return false;    
+    }
+};
+
+int main() {
+
+}
