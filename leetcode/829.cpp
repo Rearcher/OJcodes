b1f288478565c9305b089e21315f0344886364df
rahul <568813855@qq.com>
2018-05-08 14:35:09 +0800

    829. Consecutive Numbers Sum

---
+++ leetcode/829.cpp
@@ -0,0 +1,11 @@
+// 829. Consecutive Numbers Sum
+class Solution {
+public:
+    int consecutiveNumbersSum(int N) {
+        int count = 1;
+        for(int k = 2; k < sqrt(2 * N); k++) {
+            if ((N - (k * (k - 1) / 2)) % k == 0) count++;
+        }
+        return count;
+    }
+};
